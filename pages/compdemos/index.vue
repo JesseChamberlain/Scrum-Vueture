0003a921998a72ec3186ebcbee79b5f68ffd5679
brendan laughlin <blaughlin@bur-blaughli-m1.corp.endurance.com>
2019-08-28 09:12:32 -0400

    #6 created basic card component and a compdemos page where we can view and test the components we make. Also added fontawesome dep

---
+++ pages/compdemos/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-layout column justify-center align-center>
+    <sv-card vote="1" user="Brendan L." :revealed="false" />
+    <sv-card-collection />
+  </v-layout>
+</template>
+
+<script>
+import SvCard from '../../components/SvCard';
+import SvCardCollection from '../../components/SvCardCollection';
+export default {
+  name: 'SvComponentsDemos',
+  components: {
+    SvCard,
+    SvCardCollection
+  }
+};
+</script>
