0b72e65a553f66548daf93ef1a10078a87020f45
Brendan Laughlin <brendan.laughlin@endurance.com>
2019-10-18 14:23:22 -0400

    Issue #29 - added back the necessary v-content and nuxt components to actually render the page content

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,31 @@
 <template>
   <v-app dark>
-    <!-- Add header / footer components here -->
+    <!-- Add header here -->
+    <v-content>
+      <v-container>
+        <nuxt />
+      </v-container>
+    </v-content>
+    <!-- Add footer here -->
   </v-app>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      clipped: false,
+      drawer: true,
+      fixed: false,
+      items: [
+        { icon: 'apps', title: 'Welcome', to: '/' },
+        { icon: 'bubble_chart', title: 'Inspire', to: '/inspire' }
+      ],
+      miniVariant: false,
+      right: true,
+      rightDrawer: false,
+      title: 'Vuetify.js'
+    };
+  }
+};
+</script>
